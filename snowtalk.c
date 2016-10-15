9dd02d0e325ccd37df3a17cbc916d48beeac7b80
Eugene Lee <leee@mit.edu>
2016-10-15 04:55:58 -0400

    Rewritten and rewired snow talk.

--- snowtalk.c
+++ snowtalk.c
@@ -1,128 +1,63 @@
-/*************************************************** 
-  This is an example for the Adafruit VS1053 Codec Breakout
+/*
+  Code for an arduino to play the snow talk.
 
-  Designed specifically to work with the Adafruit VS1053 Codec Breakout 
-  ----> https://www.adafruit.com/products/1381
+  Based on the example for the Adafruit VS1053 Codec Breakout by Limor
+  Fried/Ladyada for Adafruit Industries (original license and text not included
+  because this is not a redistribution.
 
-  Adafruit invests time and resources providing this open source code, 
-  please support Adafruit and open-source hardware by purchasing 
-  products from Adafruit!
+  Originally modified by poohlaga, rewritten by leee after rewiring the couple.
+ */
 
-  Written by Limor Fried/Ladyada for Adafruit Industries.  
-  BSD license, all text above must be included in any redistribution
- ****************************************************/
-
-// include SPI, MP3 and SD libraries
+// Use SPI, MP3, SD libs
 #include <SPI.h>
 #include <Adafruit_VS1053.h>
 #include <SD.h>
-
-// define the pins used
-//#define CLK 13       // SPI Clock, shared with SD card
-//#define MISO 12      // Input data, from VS1053/SD card
-//#define MOSI 11      // Output data, to VS1053/SD card
-// Connect CLK, MISO and MOSI to hardware SPI pins. 
-// See http://arduino.cc/en/Reference/SPI "Connections"
 
 // These are the pins used for the breakout example
 #define BREAKOUT_RESET  9      // VS1053 reset pin (output)
 #define BREAKOUT_CS     10     // VS1053 chip select pin (output)
 #define BREAKOUT_DCS    8      // VS1053 Data/command select pin (output)
 // These are the pins used for the music maker shield
-#define SHIELD_RESET  -1      // VS1053 reset pin (unused!)
-#define SHIELD_CS     7      // VS1053 chip select pin (output)
-#define SHIELD_DCS    6      // VS1053 Data/command select pin (output)
+#define SHIELD_RESET   -1      // VS1053 reset pin (unused!)
+#define SHIELD_CS       7      // VS1053 chip select pin (output)
+#define SHIELD_DCS      6      // VS1053 Data/command select pin (output)
 
 // These are common pins between breakout and shield
-#define CARDCS 4     // Card chip select pin
+#define CARDCS          4     // Card chip select pin
 // DREQ should be an Int pin, see http://arduino.cc/en/Reference/attachInterrupt
-#define DREQ 3       // VS1053 Data request, ideally an Interrupt pin
+#define DREQ            3     // VS1053 Data request, ideally an Interrupt pin
 
-Adafruit_VS1053_FilePlayer musicPlayer = 
-  // create breakout-example object!
-  //Adafruit_VS1053_FilePlayer(BREAKOUT_RESET, BREAKOUT_CS, BREAKOUT_DCS, DREQ, CARDCS);
-  // create shield-example object!
-  Adafruit_VS1053_FilePlayer(SHIELD_RESET, SHIELD_CS, SHIELD_DCS, DREQ, CARDCS);
+Adafruit_VS1053_FilePlayer musicPlayer = Adafruit_VS1053_FilePlayer(SHIELD_RESET, SHIELD_CS, SHIELD_DCS, DREQ, CARDCS);
 
-
-int buttonA = A0;
-int buttonB = A1;
-int prevbuttonstate = 0;
+const int buttonA =    A0;
+const int buttonB =    A1;
 
 void setup() {
-  Serial.begin(9600);
-  
+  Serial.begin(9600);  // have a serial for debugging
+  Serial.println("serial initialized");
   pinMode(buttonA, INPUT);
   pinMode(buttonB, INPUT);
-  Serial.println("Adafruit VS1053 Simple Test");
-
-  if (! musicPlayer.begin()) { // initialise the music player
-     Serial.println(F("Couldn't find VS1053, do you have the right pins defined?"));
-     while (1);
+  Serial.println("buttons initialized");
+  if (! musicPlayer.begin()) {
+    Serial.println(F("Couldn't find VS1053, do you have the right pins defined?"));
+    while (1);
   }
   Serial.println(F("VS1053 found"));
-
-  SD.begin(CARDCS);    // initialise the SD card
-  
-  // Set volume for left, right channels. lower numbers == louder volume!
+  SD.begin(CARDCS);
+  Serial.println("SD card initialized");
   musicPlayer.setVolume(2,2);
-
+  Serial.println("volume set");
   // Timer interrupts are not suggested, better to use DREQ interrupt!
-  //musicPlayer.useInterrupt(VS1053_FILEPLAYER_TIMER0_INT); // timer int
+  //musicPlayer.useInterrupt(VS1053_FILEPLAYER_TIMER0_INT); // timer
 
   // If DREQ is on an interrupt pin (on uno, #2 or #3) we can do background
   // audio playing
   musicPlayer.useInterrupt(VS1053_FILEPLAYER_PIN_INT);  // DREQ int
-    /*
-
-
-  */
-
 }
 
-void loop() { 
-  int currentbuttonstate = analogRead(buttonA);
-Serial.print(currentbuttonstate);
-Serial.print(" - ");
-Serial.println(prevbuttonstate);
-  if (prevbuttonstate != currentbuttonstate && currentbuttonstate == LOW ) {
-    Serial.println("button presseD");
-      // Play one file, don't return until complete
-    Serial.println(F("Playing snow"));
-  musicPlayer.playFullFile("snow.mp3");
-  
-  
-    // Play another file in the background, REQUIRES interrupts!
-//  Serial.println(F("Playing track 002"));
-//  musicPlayer.startPlayingFile("track002.mp3");
-  }  
-  prevbuttonstate = currentbuttonstate;
-  // File is playing in the background
-  /*
-  if (musicPlayer.stopped()) {
-    Serial.println("Done playing music");
-    //while (1);
+void loop() {
+  if (digitalRead(buttonA)) {
+    Serial.println("buttonA fired");
+    musicPlayer.playFullFile("snow.mp3");
   }
-  *
-  if (Serial.available()) {
-    char c = Serial.read();
-    
-    // if we get an 's' on the serial console, stop!
-    if (c == 's') {
-      musicPlayer.stopPlaying();
-    }
-    
-    // if we get an 'p' on the serial console, pause/unpause!
-    if (c == 'p') {
-      if (! musicPlayer.paused()) {
-        Serial.println("Paused");
-        musicPlayer.pausePlaying(true);
-      } else { 
-        Serial.println("Resumed");
-        musicPlayer.pausePlaying(false);
-      }
-    }
-  }
-*/
-  delay(1000);
 }
